39d1e399b2a4cdb1a8359087f237f2c5105158e3
stanmischuk <stan.mischuk@gmail.com>
2020-03-21 13:54:11 +0200

    Initial commit

---
+++ Jobs_demo.ps1
@@ -0,0 +1,44 @@
+﻿$Source_="C:\PS_Task_demo\OnPrem"
+$Source=dir $Source_
+$Exch="C:\PS_Task_demo\StorageBucket"
+$Target="C:\PS_Task_demo\Cloud"
+$Count=(Get-ChildItem $Source_ ).Count
+Start-Job -Name j1 –ScriptBlock{
+    Param($p_Source,$p_Exch)
+
+    foreach($f in   $p_Source )
+    { 
+       Write-Host $f.name
+       Start-Sleep -s 3
+       Copy-Item $f.fullname -Destination   $p_Exch -Force
+    }
+}-ArgumentList $Source, $Exch
+ 
+ #  Code here will run async
+Start-Job -name j2  –ScriptBlock{
+    Param($p_Exch,$p_Target,$p_Count)
+
+    while ($p_Count -gt (Get-ChildItem $p_Target ).Count )
+    {
+
+        $source = dir $p_Exch
+        foreach($f in   $source )
+        { 
+           Write-Host $f.name
+           Start-Sleep -s 3
+           Move-Item $f.fullname -Destination   $p_Target -Force
+         }
+     }
+}-ArgumentList $Exch, $Target, $Count
+#  Code here will run async
+#Wait-Job -Name j1
+#Wait-Job -name j2
+#receive-Job -Name j1
+  
+#receive-Job -name j2
+#get-job -name j1
+#get-job -name j2
+#stop-job -name j2
+#stop-job -name j1
+#Remove-Job -Name j1
+#Remove-Job -Name j2
---
+++ Untitled2.ps1
@@ -0,0 +1,43 @@
+﻿$Source=dir "C:\PS_Task_demo\OnPrem"
+$Exch="C:\PS_Task_demo\StorageBucket"
+$Target="C:\PS_Task_demo\Cloud"
+Start-Job -Name j1 –ScriptBlock{
+    Param($p_Source,$p_Exch)
+    $source = $p_Source
+    $target = $p_Exch
+    foreach($f in   $source )
+    { Write-Host $f.name
+     Start-Sleep -s 3
+    Copy-Item $f.fullname -Destination   $target -Force
+    }
+     
+    #Get-ChildItem -Path $source -Destination $target -Recurse  #| Start-Sleep -s 30
+}-ArgumentList $Source, $Exch
+ #  Code here will run async
+
+Start-Job -name j2  –ScriptBlock{
+    Param($p_Exch,$p_Target)
+    while ((Get-ChildItem $p_Exch ).Count -gt 0 )
+    {
+
+        $source = dir $p_Exch
+
+    foreach($f in   $source )
+    { Write-Host $f.name
+     Start-Sleep -s 3
+    Move-Item $f.fullname -Destination   $p_Target -Force
+    }
+  }
+}-ArgumentList $Exch, $Target
+#  Code here will run async
+#Wait-Job -Name j1
+#Wait-Job -name j2
+#receive-Job -Name j1
+  
+#receive-Job -name j2
+#get-job -name j1
+#get-job -name j2
+#stop-job -name j2
+#stop-job -name j1
+#Remove-Job -Name j1
+#Remove-Job -Name j2
